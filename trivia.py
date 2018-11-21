89d73bec16babd4b3e9e7cac804d3079105e8caf
Rizwan <rsz.riz@gmail.com>
2018-11-21 13:54:45 +0530

    Further Improvements

--- trivia.py
+++ trivia.py
@@ -2,7 +2,7 @@ import requests, json, html
 import sopel.module
 def q_and_a():
 
-	response = requests.get("https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple")
+	response = requests.get("https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple")
 	data = response.json()
 	#question and answer
 	question = (html.unescape(data['results'][0]['question']))
@@ -13,41 +13,75 @@ def q_and_a():
 	for i in range(1,5):
 		an = an + ' ' + str(i) + '.' +  answers[i-1]
 
+	pr_ans = ''
+	for i in range(0,len(answers)):
+		pr_ans = pr_ans + " " + str(i+1) + " :" + str(answers[i]) + " ,"
+
 	ans = {}
 	for i in range(0,len(answers)):
 		ans[i+1] = answers[i]
-	return ans,correct_answer,question
 
-active = False
-count = 0
+	return ans,correct_answer,question,pr_ans
+
+isActive = False
+question_count = 0
 @sopel.module.commands('start')
 def start(bot,trigger):
 	#  trigger.group(2) = args
 
-	global count
-	global active
-	global ans
-	global correct_answer
-	if count < 5:
-		ans,correct_answer,question = q_and_a()
-		bot.say(question)
-		bot.say(str(ans))
-		#bot.say(correct_answer)
-		active = True
+	global question_count,isActive,ans,correct_answer
+	if not isActive:
+
+		if question_count < 5:
+			ans,correct_answer,question,pr_ans = q_and_a()
+			bot.say(question)
+			bot.say(str(pr_ans))
+			#bot.say(str(ans))
+			#bot.say(ans[1])
+			#bot.say(correct_answer)
+			isActive = True
+		else:
+			isActive = False
+	else:
+		bot.say("Quiz Already running")
 
 
-@sopel.module.rule('^[^\.\.]')
+#@sopel.module.rule('^[^\.\.]')
+@sopel.module.rule('[1-4]')
+
 def an(bot, trigger):
-	global active
-	global count
-	if (active == True) and (str(trigger) == correct_answer):
-		bot.say("Congratulations " + str(trigger.nick) + " You win")
-		active = False
-		count = count + 1
-		start(bot,trigger)
+	global isActive,question_count,ans
 
+#	if (isActive == True) and (str(trigger) == correct_answer):
+
+	if (isActive == True) and (ans[int(trigger)] == correct_answer):
+		bot.say("Congratulations " + str(trigger.nick) + " You win")
+		isActive = False
+		question_count += 1
+		if question_count < 5:
+			start(bot,trigger)
+		else:
+			bot.say("Game Over")
+			question_count = 0
 	#	if ans[int(trigger)] == correct_answer:
 	#		bot.say("Correct")
+
+
+@sopel.module.commands('skip')
+def skip(bot,trigger):
+	global question_count,correct_answer,isActive
+
+	if isActive == True:
+
+		bot.say("Question Skipped, The answer was " + correct_answer)
+		question_count += 1
+
+		isActive = False
+		if question_count == 5:
+			bot.say("Game Over")
+		start(bot, trigger)
+	else:
+		bot.say("Quiz Not Running, Can't Skip")
 
 
 
